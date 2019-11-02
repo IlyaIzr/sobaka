54c6e7bef737e059761479a6a4ef0b41c1ff1ad7
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2019-11-02 22:51:18 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig, {userProfile: 'users', useFirestoreForProfile: true, attachAuthIsReady: true}) // redux binding to firebase
+    // redux bindings for firestore
+  )
+);  
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+  registerServiceWorker();
+});
+
+let nav_Buttons = document.getElementsByClassName("nav-link");
+
+console.log(nav_Buttons);
+if(nav_Buttons){
+[...nav_Buttons].forEach(element => {
+  console.log(element);
+  //element.addEventListener('click', function(){
+  //  document.getElementsByClassName("navbar-toggler")[0].className += " collapsed";
+  //  document.getElementsByClassName("navbar-collapse")[0].classList.remove("show");    
+  //});
+});
+}
